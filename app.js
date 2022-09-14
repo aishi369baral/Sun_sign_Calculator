729b55e4c00305f8af1b84cef97748980a21a601
aishi369baral <aishi369baral@gmail.com>
2022-09-14 20:37:20 +0530

    2nd commit

--- app.js
+++ app.js
@@ -185,6 +185,14 @@ else{
 
 });
 
-app.listen(3000, function () {
-    console.log("Server Started at Port 3000.");
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+
+app.listen(port, function () {
+    console.log("Server has started successfully.");
 });
